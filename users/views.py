d50b6c7d4bd27f158e9aa77f0fbf97ab7b8cc709
Erhan <erhantezer68@gmail.com>
2022-10-04 16:20:07 +0300

    updated

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializers
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
 from rest_framework import generics
@@ -9,5 +10,14 @@ from rest_framework import generics
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializers
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_expeption=True)
+        user=serializer.save()
+        data=serializer.data
+        if Token.objects.filter(user=user).exists():
+            token=Token.objects.get(user=user)
+            data["token"] = token.key
 
 
