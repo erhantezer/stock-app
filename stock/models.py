273fb0f3fa8f39c061654fce9e323b33493e391f
Erhan <erhantezer68@gmail.com>
2022-10-05 13:40:06 +0300

    updated

--- stock/models.py
+++ stock/models.py
@@ -1,4 +1,4 @@
-from tkinter import CASCADE
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,16 +6,26 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
     
+    def __str_(self):
+        return self.category_name
+    class Meta:
+        verbose_name_plural = "Categories"
 
 class Brand(models.Model):
     brand_name = models.CharField(max_length=50)
+    def __str_(self):
+        return self.brand_name
     
 
 class Product(models.Model):
     product_name = models.CharField(max_length=50)
-    category = models.ForeignKey(Category, on_delete=CASCADE)
-    brand = models.ForeignKey(Brand, on_delete=CASCADE)
-    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    stock_quantity = models.IntegerField()
+    
+    def __str_(self):
+        return f"{self.brand} {self.brand_name}'un stock miktarı {self.stock} tanedir"
+    
     
     
 class Firm(models.Model):
@@ -23,10 +33,24 @@ class Firm(models.Model):
     phone_number = models.IntegerField()   
     address = models.CharField(max_length=50)
     
+    def __str_(self):
+        return f"{self.brand_name}"
+    
     
 class Stock(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE)
-    firm = models.ForeignKey(Firm, on_delete=CASCADE)
-    transaction = models.CharField(max_length=50)
-    product = models.ForeignKey(Product, on_delete=CASCADE)
+    TRANSACTION_TYPE = [
+        ("I","IN"),
+        ("O","ON"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    firm = models.ForeignKey(Firm, on_delete=models.CASCADE)
+    transaction = models.IntegerField(max_length=1, choices=TRANSACTION_TYPE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_product_date = models.DateTimeField(auto_now=True)
+    remove_product_date = models.DateTimeField(auto_now=True)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price_total = models.CharField(max_length=15,)
 
+    def __str_(self):
+        return f"{self.product} {self.user} tarafından {self.transaction} işlemi yapıldı"
