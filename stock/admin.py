cf351602051473cd24efff0d603ca2c4503e3d6a
Erhan <erhantezer68@gmail.com>
2022-10-05 13:04:12 +0300

    updated

--- stock/admin.py
+++ stock/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 
+from stock.models import (
+    Brand, 
+    Category, 
+    Firm, 
+    Product, 
+    Stock
+    )
+
 # Register your models here.
-# admin.site.register()
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Firm)
+admin.site.register(Brand)
+admin.site.register(Stock)
--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,32 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=50)
+    
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=CASCADE)
+    stock = models.IntegerField()
+    
+    
+class Firm(models.Model):
+    firm_name = models.CharField(max_length=50) 
+    phone_number = models.IntegerField()   
+    address = models.CharField(max_length=50)
+    
+    
+class Stock(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    firm = models.ForeignKey(Firm, on_delete=CASCADE)
+    transaction = models.CharField(max_length=50)
+    product = models.ForeignKey(Product, on_delete=CASCADE)
+
